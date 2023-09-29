9cf94141a0c74399664ca55550a46183c32ff827
rakslice <rakslice@gmail.com>
2023-09-28 20:17:07 -0700

    initial commit

---
+++ buildscripts/template.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+set -x
+
+package=libast-0.7
+
+. preamble.inc
+
+#cp $codefragments/strcasecmp.c src/lib/
+
+#config_libs="-lm -lz -ljpeg -ltiff"
+#config_options="PNG_LIBS=-lpng16 PNG_CFLAGS=-I/usr/local/include/libpng16"
+
+if [ ! -f .patched ]; then
+	patch -p1 -i $patches/$package.patch
+	touch .patched
+fi
+
+CONFIG_SHELL=/usr/local/bin/bash
+export CONFIG_SHELL
+
+. $incdir/build.inc
+
+unset CONFIG_SHELL
+
---
+++ buildscripts/windowmaker.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+set -x
+
+package=WindowMaker-0.70.0
+
+. preamble.inc
+
+cp $codefragments/strcasecmp.c WINGs/
+
+if [ ! -f .patched ]; then
+	patch -p1 -i $patches/$package.patch
+	touch .patched
+fi
+
+CONFIG_SHELL=/usr/local/bin/bash
+export CONFIG_SHELL
+
+. $incdir/build.inc
+
+unset CONFIG_SHELL
+
