8e33a7696919e7b1a7e812f283d801907deec7c0
rakslice <rakslice@gmail.com>
2023-09-28 20:43:16 -0700

    more build scripts

---
+++ buildscripts/libpng-1.6.34.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+set -x
+
+package=libpng-1.6.34
+
+. preamble.inc
+
+#cp $codefragments/strcasecmp.c src/lib/
+
+#config_libs="-lm -lz -ljpeg -ltiff"
+config_options="--enable-mips-msa=no --enable-powerpc-vsx=no"
+
+ACCEPT_INFERIOR_RM_PROGRAM=yes
+export ACCEPT_INFERIOR_RM_PROGRAM
+
+#if [ ! -f .patched ]; then
+#	patch -p1 -i $patches/$package.patch
+#	touch .patched
+#fi
+
+CONFIG_SHELL=/usr/local/bin/bash
+export CONFIG_SHELL
+
+. $incdir/build.inc
+
+unset CONFIG_SHELL
+unset ACCEPT_INFERIOR_RM_PROGRAM
+
