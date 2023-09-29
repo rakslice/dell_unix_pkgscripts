7e541a724ffd47f63bf749f588c8ee3e9a7afc10
rakslice <rakslice@gmail.com>
2023-09-29 01:29:44 -0700

    make 3.7x

---
+++ buildscripts/make.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+set -x
+
+package=make-3.76.1
+
+PATH=/usr/local/bin:/usr/dell/gcc-2/bin:$PATH
+export PATH
+
+. preamble.inc
+
+#cp $codefragments/strcasecmp.c src/lib/
+
+#config_libs="-lm -lz -ljpeg -ltiff"
+#config_options="--disable-nls --enable-languages=c,c++"
+
+if [ ! -f .patched ]; then
+	patch -p1 < $patches/$package.patch
+	touch .patched
+fi
+
+CONFIG_SHELL=/usr/local/bin/bash
+export CONFIG_SHELL
+
+
+. $incdir/build.inc
+
+unset CONFIG_SHELL
+
