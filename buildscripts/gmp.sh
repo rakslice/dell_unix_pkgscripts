ae7a3297aaf0c599f113e9a2f451e87d3db0fd56
rakslice <rakslice@gmail.com>
2023-10-04 17:05:36 -0700

    these are newer gmp mpc mpfr for gcc 4.8.3, just parking these for now

---
+++ buildscripts/gmp.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+set -x
+
+package=gmp-4.3.2
+
+PATH=/usr/local/bin:$PATH
+export PATH
+
+. preamble.inc
+
+config_options=""
+
+CONFIG_SHELL=/usr/local/bin/bash
+export CONFIG_SHELL
+
+. $incdir/build.inc
+
+unset CONFIG_SHELL
+
---
+++ buildscripts/mpc.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+set -x
+
+package=mpc-1.0.1
+
+PATH=/usr/local/bin:$PATH
+export PATH
+
+. preamble.inc
+
+config_options=""
+
+ACCEPT_INFERIOR_RM_PROGRAM=yes
+export ACCEPT_INFERIOR_RM_PROGRAM
+
+CONFIG_SHELL=/usr/local/bin/bash
+export CONFIG_SHELL
+
+. $incdir/build.inc
+
+unset CONFIG_SHELL
+
