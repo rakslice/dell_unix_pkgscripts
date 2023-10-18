8d60146ef2a53a6438d136987a2f2571c16f8155
rakslice <rakslice@gmail.com>
2023-10-18 02:47:15 -0700

    pkg libpng

--- buildscripts/libpng-1.6.34.sh
+++ buildscripts/libpng-1.6.34.sh
@@ -9,23 +9,20 @@ export PATH
 
 . preamble.inc
 
-#cp $codefragments/strcasecmp.c src/lib/
-
-#config_libs="-lm -lz -ljpeg -ltiff"
 config_options="--enable-mips-msa=no --enable-powerpc-vsx=no"
 
 ACCEPT_INFERIOR_RM_PROGRAM=yes
 export ACCEPT_INFERIOR_RM_PROGRAM
 
-#if [ ! -f .patched ]; then
-#	patch -p1 -i $patches/$package.patch
-#	touch .patched
-#fi
-
 CONFIG_SHELL=/usr/local/bin/bash
 export CONFIG_SHELL
 
+configure_cmd="bash configure"
+
 . $incdir/build.inc
+
+prefixvar=DESTDIR
+pkgdestrootbased=1
 
 unset CONFIG_SHELL
 unset ACCEPT_INFERIOR_RM_PROGRAM
