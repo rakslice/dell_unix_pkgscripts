4be2f1b6a9e8e0e7d2fdd73ad1e54b13de045b3a
rakslice <rakslice@gmail.com>
2023-10-18 05:36:29 -0700

    pkg sed

--- buildscripts/sed.sh
+++ buildscripts/sed.sh
@@ -9,11 +9,6 @@ export PATH
 
 . preamble.inc
 
-#cp $codefragments/strcasecmp.c src/lib/
-
-#config_libs="-lm -lz -ljpeg -ltiff"
-#config_options="PNG_LIBS=-lpng16 PNG_CFLAGS=-I/usr/local/include/libpng16"
-
 if [ ! -f .patched ]; then
 	patch -p1 -i $patches/$package.patch
 	touch .patched
@@ -26,6 +21,9 @@ SHELL=/usr/local/bin/bash
 export SHELL
 
 configure_cmd="bash configure"
+
+prefixvar=DESTDIR
+pkgdestrootbased=1
 
 . $incdir/build.inc
 
