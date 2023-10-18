361e35e46825913c8487fb48883dd654dba862c6
rakslice <rakslice@gmail.com>
2023-10-18 01:53:27 -0700

    pkg gzip

--- buildscripts/gzip.sh
+++ buildscripts/gzip.sh
@@ -6,10 +6,7 @@ package=gzip-1.2.4
 
 . preamble.inc
 
-#cp $codefragments/strcasecmp.c src/lib/
-
-#config_libs="-lm -lz -ljpeg -ltiff"
-#config_options="PNG_LIBS=-lpng16 PNG_CFLAGS=-I/usr/local/include/libpng16"
+prefixvar=prefix
 
 . $incdir/build.inc
 
