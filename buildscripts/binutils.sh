2fa7fe2aa1763df2a038c024045bf2dae1d0876d
rakslice <rakslice@gmail.com>
2023-10-17 17:59:50 -0700

    package building support; packaging of binutils, gcc-3.0.4

--- buildscripts/binutils.sh
+++ buildscripts/binutils.sh
@@ -8,16 +8,6 @@ use_old_tar=1
 
 . preamble.inc
 
-#cp $codefragments/strcasecmp.c src/lib/
-
-#config_libs="-lm -lz -ljpeg -ltiff"
-#config_options="--disable-nls --enable-languages=c,c++"
-
-#if [ ! -f .patched ]; then
-#	patch -p1 -i $patches/$package.patch
-#	touch .patched
-#fi
-
 cd ..
 if [ ! -d build-binutils ]; then
 	mkdir build-binutils
@@ -25,6 +15,10 @@ fi
 cd build-binutils
 
 configure_cmd=../$package/configure
+
+configure_once=1
+
+prefixvar=prefix
 
 CONFIG_SHELL=/usr/local/bin/bash
 export CONFIG_SHELL
--- buildscripts/gcc-3.0.4.sh
+++ buildscripts/gcc-3.0.4.sh
@@ -2,12 +2,19 @@
 set -e
 set -x
 
-package=binutils-2.13.2
+package=gcc-3.0.4
 
 PATH=/usr/local/bin:$PATH
 export PATH
 
 . preamble.inc
+
+config_options="--disable-nls --enable-languages=c,c++"
+
+if [ ! -f .patched ]; then
+	patch -p1 < $patches/$package.patch
+	touch .patched
+fi
 
 cd ..
 builddir=build-$package
@@ -20,7 +27,7 @@ configure_cmd=../$package/configure
 
 configure_once=1
 
-prefixvar=DESTDIR
+prefixvar=prefix
 
 CONFIG_SHELL=/usr/local/bin/bash
 export CONFIG_SHELL
