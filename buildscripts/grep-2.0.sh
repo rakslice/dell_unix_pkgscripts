37acab7a01b4f9f34e399ff1f6e006c1e2adc8ee
rakslice <rakslice@gmail.com>
2023-10-18 04:55:46 -0700

    package grep

--- buildscripts/grep-2.0.sh
+++ buildscripts/grep-2.0.sh
@@ -11,5 +11,13 @@ use_old_tar=1
 PATH=/usr/dell/gcc-2/bin:$PATH
 export PATH
 
+prefixvar=prefix
+
+for f in bin man/man1; do
+	if [ ! -d $srcdir/destdir/$package/$f ]; then
+		mkdir -p $srcdir/destdir/$package/$f
+	fi
+done
+
 . $incdir/build.inc
 
