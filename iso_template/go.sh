fd42d0c47deda8d21c0a875bc88a4ba39581a792
rakslice <rakslice@gmail.com>
2023-10-24 01:56:26 -0700

    use alt location for built packages; cleanup pkgdestdir after build; cleanup built source directory if running from go.sh

--- iso_template/go.sh
+++ iso_template/go.sh
@@ -3,6 +3,7 @@ set -e
 set -x
 
 storage_location=/home2
+pkg_storage=/home3/pkg
 read pause
 
 [ -d $storage_location ]
@@ -13,7 +14,13 @@ if [ ! -d $working_dir ]; then
 	mkdir $working_dir
 fi
 
+if [ ! -d $pkg_storage ]; then
+	mkdir $pkg_storage
+fi
+
 ln -s $working_dir /home/dell
+
+ln -s $pkg_storage /home/dell/pkg
 
 if [ ! -d /usr/local ]; then
 	mkdir $storage_location/local
@@ -30,5 +37,8 @@ if [ ! -d $working_dir/buildscripts ]; then
 fi
 
 cd $working_dir/buildscripts
+
+REMOVE_BUILT_SOURCE_DIRS=1
+export REMOVE_BUILT_SOURCE_DIRS
 
 ./all.sh
