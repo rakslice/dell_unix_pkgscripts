a8d665216b00390f9e78084d9ddc0b5bd91e45c2
rakslice <rakslice@gmail.com>
2023-11-10 21:16:45 -0800

    updated to jpeg 9e since it has all the autotools inputs so i can libtoolize

---
+++ buildscripts/jpeg-9e.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -e
+set -x
+
+package=jpeg-9e
+
+tarball_prefix=jpegsrc.v9e.tar
+
+PATH=/usr/local/bin:$PATH
+export PATH
+
+. preamble.inc
+
+config_options="--enable-static --enable-shared"
+
+CONFIG_SHELL=/usr/local/bin/bash
+export CONFIG_SHELL
+
+ACCEPT_INFERIOR_RM_PROGRAM=yes
+export ACCEPT_INFERIOR_RM_PROGRAM
+
+# for dynamic libraries
+libtoolize
+autoreconf
+
+. $incdir/build.inc
+
+unset CONFIG_SHELL
+
